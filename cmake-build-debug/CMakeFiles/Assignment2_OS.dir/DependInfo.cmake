a2932a66d65c262597f5128d0798948249816499
UnoDosAndres <s205450@student.dtu.dk>
2021-11-03 20:50:37 +0100

    readme

---
+++ cmake-build-debug/CMakeFiles/Assignment2_OS.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/andre/CLionProjects/Assignment2-OS/Mandatory Assignment 2/memorytests.c" "C:/Users/andre/CLionProjects/Assignment2-OS/cmake-build-debug/CMakeFiles/Assignment2_OS.dir/Mandatory_Assignment_2/memorytests.c.obj"
+  "C:/Users/andre/CLionProjects/Assignment2-OS/Mandatory Assignment 2/mymem.c" "C:/Users/andre/CLionProjects/Assignment2-OS/cmake-build-debug/CMakeFiles/Assignment2_OS.dir/Mandatory_Assignment_2/mymem.c.obj"
+  "C:/Users/andre/CLionProjects/Assignment2-OS/Mandatory Assignment 2/testrunner.c" "C:/Users/andre/CLionProjects/Assignment2-OS/cmake-build-debug/CMakeFiles/Assignment2_OS.dir/Mandatory_Assignment_2/testrunner.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Mandatory Assignment 2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Assignment2_OS.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "Assignment2_OS.exe"
+  "Assignment2_OS.exe.manifest"
+  "Assignment2_OS.pdb"
+  "CMakeFiles/Assignment2_OS.dir/Mandatory_Assignment_2/memorytests.c.obj"
+  "CMakeFiles/Assignment2_OS.dir/Mandatory_Assignment_2/mymem.c.obj"
+  "CMakeFiles/Assignment2_OS.dir/Mandatory_Assignment_2/testrunner.c.obj"
+  "libAssignment2_OS.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Assignment2_OS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
